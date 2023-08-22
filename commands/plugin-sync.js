ef02604aabf0bc3e5a3111a16d3f4a524f47a431
Nick Galvez <nicholas.galvez@gmail.com>
2023-08-22 15:27:36 -0700

    lint

--- commands/plugin-sync.js
+++ commands/plugin-sync.js
@@ -8,7 +8,7 @@ module.exports = {
   handler: (argv) => {
     console.log(process.env.SITE_NAME) // This is available because of the loadEnvMiddleware
 
-    const command = join(__dirname, '..', 'bin', 'composer-sync-plugins.sh');
+    const command = join(__dirname, '..', 'bin', 'composer-sync-plugins.sh')
 
     const child = spawn(command, {
       stdio: 'inherit'
